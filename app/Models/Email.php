a4b88427b789dbf120be7941472974a5f93bf415
JasonYan <jasonyan1031@foxmail.com>
2025-06-18 18:11:07 +0800

    perf: 减少email模型不必要的数据传输

--- app/Models/Email.php
+++ app/Models/Email.php
@@ -12,8 +12,32 @@ class Email extends Model
         'bcc' => 'array',
     ];
 
+    protected $hidden = [
+        "raw_headers",
+    ];
+
     public function attachments()
     {
         return $this->hasMany(Attachment::class);
+    }
+
+    public function newQuery()
+    {
+        // 不获取'raw_headers'字段
+        return parent::newQuery()->select([
+            'id',
+            'subject',
+            'message_id',
+            'sender',
+            'recipients',
+            'cc',
+            'bcc',
+            'content_text',
+            'content_html',
+            'date_sent',
+            'date_received',
+            'created_at',
+            'updated_at',
+        ]);
     }
 }
