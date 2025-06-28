af3434ce35044b0056371dcd354453aad1b62f56
JasonYan <jasonyan1031@foxmail.com>
2025-06-28 20:46:08 +0800

    feat: 为请求添加相应的权限校验

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AttachmentController;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\EmailController;
+use App\Http\Controllers\EmailRuleController;
 use App\Http\Controllers\SalerController;
 use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
@@ -20,28 +21,33 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::post('/logout-all', [AuthController::class, 'logoutAll']);
         Route::post('/refresh-token', [AuthController::class, 'refreshToken']);
     });
+
+    Route::prefix('/salers')->middleware('role:admin,dispatcher')->group(function () {
+        Route::post('/', [SalerController::class, 'index']);
+        Route::post('/store', [SalerController::class, 'store']);
+        Route::post('/show/{saler}', [SalerController::class, 'show']);
+        Route::post('/update/{saler}', [SalerController::class, 'update']);
+        Route::post('/destroy/{saler}', [SalerController::class, 'destroy']);
+    });
+
+    Route::prefix('/tags')->middleware('role:admin,dispatcher')->group(function () {
+        Route::post('/', [TagController::class, 'index']);
+        Route::post('/store', [TagController::class, 'store']);
+        Route::post('/show/{tag}', [TagController::class, 'show']);
+        Route::post('/update/{tag}', [TagController::class, 'update']);
+        Route::post('/destroy/{tag}', [TagController::class, 'destroy']);
+    });
+
+    Route::prefix('/emails')->middleware('role:admin,dispatcher')->group(function () {
+        Route::post('/', [EmailController::class, 'index']);
+    });
+
+    Route::prefix('/email-rules')->middleware('role:admin')->group(function () {
+        Route::post('/', [EmailRuleController::class, 'index']);
+    });
 });
 
-Route::prefix('/salers')->group(function () {
-    Route::post('/', [SalerController::class, 'index']);
-    Route::post('/store', [SalerController::class, 'store']);
-    Route::post('/show/{saler}', [SalerController::class, 'show']);
-    Route::post('/update/{saler}', [SalerController::class, 'update']);
-    Route::post('/destroy/{saler}', [SalerController::class, 'destroy']);
-});
-
-Route::prefix('/tags')->group(function () {
-    Route::post('/', [TagController::class, 'index']);
-    Route::post('/store', [TagController::class, 'store']);
-    Route::post('/show/{tag}', [TagController::class, 'show']);
-    Route::post('/update/{tag}', [TagController::class, 'update']);
-    Route::post('/destroy/{tag}', [TagController::class, 'destroy']);
-});
-
-Route::prefix('/emails')->group(function () {
-    Route::post('/', [EmailController::class, 'index']);
-});
-
+// TODO: 解决附件查看的权限保护
 Route::prefix('/email-attachments')->group(function () {
     Route::get('/cids/{cid}', [AttachmentController::class, 'showByCid']);
     Route::get('/{attachment}', [AttachmentController::class, 'show']);
