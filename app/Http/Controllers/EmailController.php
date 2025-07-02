c5885da543a3cbe31917c1fd6bdc140c033fa03b
JasonYan <jasonyan1031@foxmail.com>
2025-07-02 17:26:08 +0800

    feat: 实现邮件列表分页与按日期筛选

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -7,12 +7,19 @@ use Illuminate\Http\Request;
 
 class EmailController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $emails = Email::with('attachments')
+        $query = Email::with('attachments')
             ->with('forwards')
-            ->latest('date_sent')
-            ->get();
+            ->latest('date_sent');
+
+        // 根据发信日期筛选
+        if ($request->filled('date_sent')) {
+            $query->whereBetween('date_sent', [$request->date_sent . ' 00:00:00', $request->date_sent . ' 23:59:59']);
+        }
+
+        // 数据分页
+        $emails = $query->paginate($request->pageSize ?? 10, ['*'], 'current');
 
         return response()->json([
             'data' => $emails,
